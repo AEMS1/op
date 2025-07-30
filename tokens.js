284c3e5e2590299a08b2be519de920bbb3384730
AEMS1 <aems91806@gmail.com>
2025-07-31 03:03:28 +0330

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,14 +1,6 @@
 const tokens = [
-    {
-      symbol: "BNB",
-      address: "bnb" // فقط همین مقدار برای تشخیص Native
-    },
-    {
-      symbol: "USDT",
-      address: "0x55d398326f99059ff775485246999027b3197955"
-    },
-    {
-      symbol: "LGD",
-      address: "0x4751C0DE56EFB3770615097347cbF131D302498A"
-    }
-  ];
+  { symbol: "BNB", address: "bnb" },
+  { symbol: "USDT", address: "0x55d398326f99059ff775485246999027b3197955" },
+  { symbol: "LGD", address: "0x4751C0DE56EFB3770615097347cbF131D302498A" },
+  { symbol: "ETH", address: "0x2170ed0880ac9a755fd29b2688956bd959f933f8" }
+];
