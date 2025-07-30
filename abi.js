c58b326d2f99da261296e2cc862c67292c1ae917
AEMS1 <aems91806@gmail.com>
2025-07-31 03:04:41 +0330

    Update abi.js

--- abi.js
+++ abi.js
@@ -1,43 +1,6 @@
 const legendSwapRouterABI = [
   {
     "inputs": [
-      { "internalType": "address", "name": "_router", "type": "address" },
-      { "internalType": "address", "name": "_rewardContract", "type": "address" },
-      { "internalType": "address", "name": "_owner", "type": "address" }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-  },
-  {
-    "inputs": [],
-    "name": "FEE_DENOMINATOR",
-    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "feePercent",
-    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "owner",
-    "outputs": [{ "internalType": "address", "name": "", "type": "address" }],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "rewardContract",
-    "outputs": [{ "internalType": "address", "name": "", "type": "address" }],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
       { "internalType": "uint256", "name": "amountOutMin", "type": "uint256" },
       { "internalType": "address[]", "name": "path", "type": "address[]" },
       { "internalType": "uint256", "name": "deadline", "type": "uint256" }
@@ -73,37 +36,14 @@ const legendSwapRouterABI = [
   }
 ];
 
-const swapABI = [
+const pancakeRouterABI = [
   {
     "inputs": [
-      { "internalType": "address", "name": "fromToken", "type": "address" },
-      { "internalType": "address", "name": "toToken", "type": "address" },
-      { "internalType": "uint256", "name": "amountIn", "type": "uint256" }
+      { "internalType": "uint256", "name": "amountIn", "type": "uint256" },
+      { "internalType": "address[]", "name": "path", "type": "address[]" }
     ],
-    "name": "swap",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      { "internalType": "address", "name": "toToken", "type": "address" }
-    ],
-    "name": "swapFromBNB",
-    "outputs": [],
-    "stateMutability": "payable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      { "internalType": "address", "name": "fromToken", "type": "address" },
-      { "internalType": "address", "name": "toToken", "type": "address" },
-      { "internalType": "uint256", "name": "amountIn", "type": "uint256" }
-    ],
-    "name": "estimateOut",
-    "outputs": [
-      { "internalType": "uint256", "name": "amountOut", "type": "uint256" }
-    ],
+    "name": "getAmountsOut",
+    "outputs": [{ "internalType": "uint256[]", "name": "amounts", "type": "uint256[]" }],
     "stateMutability": "view",
     "type": "function"
   }
@@ -118,26 +58,6 @@ const erc20ABI = [
     ],
     "name": "approve",
     "outputs": [{ "name": "", "type": "bool" }],
-    "type": "function"
-  }
-];
-
-const rewardDistributorABI = [
-  {
-    "inputs": [],
-    "name": "claimReward",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }
-];
-
-const rewardDistributorABI = [
-  {
-    "inputs": [],
-    "name": "claimReward",
-    "outputs": [],
-    "stateMutability": "nonpayable",
     "type": "function"
   }
 ];
