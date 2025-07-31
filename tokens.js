0d79e3023a3fa6b2d6679e8ffb9b8ec0f03fa763
AEMS1 <aems91806@gmail.com>
2025-07-31 04:11:46 +0330

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,6 +1,6 @@
 const tokens = [
   { symbol: "BNB", address: "bnb" },
   { symbol: "USDT", address: "0x55d398326f99059ff775485246999027b3197955" },
-  { symbol: "D", address: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c" },
+  { symbol: "D", address: "0xF21768cCBC73Ea5B6fd3C687208a7c2def2d966e" },
   { symbol: "ETH", address: "0x2170ed0880ac9a755fd29b2688956bd959f933f8" }
 ];
